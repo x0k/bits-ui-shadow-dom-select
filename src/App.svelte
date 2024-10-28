cc064a53bcfe53ca950227848385a4b879f6b0a6
Krasilnikov Roman <x0kill@ya.ru>
2024-10-28 22:07:25 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { ShadowHost } from './lib/shadow'
+  import Select from './lib/Select.svelte';
+</script>
+
+<main>
+  <ShadowHost>
+    <Select />
+  </ShadowHost>
+</main>

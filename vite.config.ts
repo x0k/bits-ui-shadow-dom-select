0983e04ade7f59f8c2d29db4387654f89b8dce47
Krasilnikov Roman <x0kill@ya.ru>
2024-10-28 22:10:02 +0300

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bits-ui-shadow-dom-select/',
   plugins: [svelte()],
 })
